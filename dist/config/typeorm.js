c7b216f03356a8f296cf926f8cdc54ab8f64e616
gaston-m <gaston.munoz.tandil@gmail.com>
2020-10-08 03:53:45 -0300

    TO deploy

---
+++ dist/config/typeorm.js
@@ -0,0 +1,44 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.connect = void 0;
+require("reflect-metadata");
+const path_1 = __importDefault(require("path"));
+const typeorm_1 = require("typeorm");
+const dotenv_1 = __importDefault(require("dotenv"));
+const signale_1 = __importDefault(require("signale"));
+dotenv_1.default.config();
+exports.connect = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield typeorm_1.createConnection({
+            type: 'postgres',
+            host: process.env.PG_HOST,
+            port: Number(process.env.PG_PORT),
+            username: process.env.PG_USER,
+            password: process.env.PG_PASS,
+            database: process.env.PG_DATABASE,
+            synchronize: true,
+            ssl: {
+                rejectUnauthorized: false
+            },
+            entities: [
+                path_1.default.join(__dirname, 'entity/**.js')
+            ],
+        });
+        signale_1.default.success("PostgreSQL Cloud is connected");
+    }
+    catch (error) {
+        signale_1.default.error(`PostgreSQL Cloud connection error - ${error.message}`);
+    }
+});
---
+++ dist/graphql/schema.js
@@ -0,0 +1,133 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const apollo_server_1 = require("apollo-server");
+const typeDefs = apollo_server_1.gql `
+
+    # User types
+
+    type User {
+        id: ID
+        name: String
+        email: String
+        createdAt: String
+    }
+
+    type UserToken {
+        user: User
+        token: String
+    }
+
+    input UserInputUpdate {
+        id: ID!
+        name: String
+        email: String
+        password: String
+    }
+
+    input UserInput {
+        name: String!
+        email: String!
+        password: String!
+    }
+
+    input UserLoginInput {
+        email: String
+        password: String
+    }
+
+    # Categories Type
+
+    type Category {
+        id: ID
+        name: String
+    }
+
+    input NewCategoryInput {
+        name: String!
+    }
+
+    input updateCategoryInput {
+        id: ID!
+        name: String!
+    }
+
+    input filterCategory {
+        name: String
+    }
+
+    # Recipe Types
+
+    type Recipe {
+        id: ID
+        name: String!
+        description: String!
+        ingredients: String!
+        user: User 
+        category: Category
+    }
+
+    input CreateRecipeInput {
+        name: String!
+        description: String!
+        ingredients: String!
+        categoryId: Int!
+    }
+    
+    input UpdateRecipeInput {
+        id: ID!
+        name: String
+        description: String
+        ingredients: String
+        userId: Int
+        category: Int
+    }
+
+    input FilterRecipe {
+        name: String
+        description: String
+        ingredients: String
+        category: Int
+    }
+
+
+    # main
+
+    type Query {
+        # User
+        getUser(token: String!): User
+        users: [User]
+
+        # Category
+        getCategories(filter: filterCategory): [Category] 
+        getOneCategory(id: ID!): Category
+          # userCategories(userId: ID!): [Categories]
+
+        # Recipe
+        getRecipes(filter: FilterRecipe): [Recipe]
+        getMyRecipes(filter: FilterRecipe): [Recipe]
+        getOneRecipe(id: ID!): Recipe   
+    }
+
+    
+    type Mutation {
+        # User
+        signUp(user: UserInput!): User
+        login(user: UserLoginInput!): UserToken
+        deleteUser(id: ID!): Boolean
+        updateUser(user: UserInputUpdate!): User
+
+
+        # Category
+        createCategory(input: NewCategoryInput!): Category
+        deleteCategory(id: ID!): Category  
+        updateCategory(category: updateCategoryInput!): Category
+
+        # Recipes 
+
+        createRecipe(recipe: CreateRecipeInput!): Recipe
+        updateRecipe(recipe: UpdateRecipeInput!): Recipe
+        deleteRecipe(id: ID!): Recipe
+
+    }
+`;
+exports.default = typeDefs;
